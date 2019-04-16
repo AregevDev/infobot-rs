9d70c00bd3fbcdd89599fc5d6a5ae0002d9f1695
AregevDev <aregevdev@gmail.com>
2019-04-16 17:12:44 +0300

    A simple bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::{
+    client::{Context, EventHandler},
+    command,
+    framework::StandardFramework,
+    model::gateway::Ready,
+    Client,
+};
+
+const TOKEN: &str = "NTY3MDU3NTk0NjI4NDQwMDcw.XLXbIA.B1kC-NELIBVah_roP5w4caZQsrI";
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _ctx: Context, _data_about_bot: Ready) {
+        println!("Bot is ready!");
+    }
 }
+
+fn main() {
+    let mut client = Client::new(TOKEN, Handler).unwrap();
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("~").allow_dm(false).case_insensitivity(true))
+            .cmd("info", Info),
+    );
+
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+command!(Info(_context, message) {
+    let _ = message.reply("Pong!");
+});
