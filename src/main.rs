e63cd2fac458b13a5fd104bfb2d2dbff44e7d4af
AregevDev <aregevdev@gmail.com>
2019-04-16 21:07:25 +0300

    The basic bot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use chrono::NaiveDateTime;
 use serenity::{
     client::{Context, EventHandler},
     command,
     framework::StandardFramework,
-    model::gateway::Ready,
+    model::{gateway::Game, gateway::Ready, guild::Member, misc::Mentionable, user::OnlineStatus},
     Client,
 };
 
@@ -11,8 +12,12 @@ const TOKEN: &str = "NTY3MDU3NTk0NjI4NDQwMDcw.XLXbIA.B1kC-NELIBVah_roP5w4caZQsrI
 struct Handler;
 
 impl EventHandler for Handler {
-    fn ready(&self, _ctx: Context, _data_about_bot: Ready) {
-        println!("Bot is ready!");
+    fn ready(&self, _ctx: Context, bot: Ready) {
+        _ctx.shard.set_status(OnlineStatus::Online);
+        _ctx.shard.set_game(Some(Game::playing(
+            format!("Informing in {} servers!", bot.guilds.len()).as_str(),
+        )));
+        println!("Bot is ready: {}#{}", bot.user.name, bot.user.discriminator);
     }
 }
 
@@ -20,8 +25,8 @@ fn main() {
     let mut client = Client::new(TOKEN, Handler).unwrap();
     client.with_framework(
         StandardFramework::new()
-            .configure(|c| c.prefix("~").allow_dm(false).case_insensitivity(true))
-            .cmd("info", Info),
+            .configure(|c| c.prefix("~"))
+            .cmd("info", info),
     );
 
     if let Err(why) = client.start() {
@@ -29,6 +34,42 @@ fn main() {
     }
 }
 
-command!(Info(_context, message) {
-    let _ = message.reply("Pong!");
+command!(info(_ctx, msg) {
+    msg.channel_id.broadcast_typing().unwrap();
+    let member: Member = msg.guild_id.unwrap().member(msg.author.id).unwrap();
+
+    let username = msg.clone().author.name;
+    let nickname = member.display_name();
+    let discriminator = msg.author.discriminator;
+    let id = msg.author.id;
+    let avatar_url = msg.author.avatar_url().unwrap();
+    let joined_at = NaiveDateTime::from_timestamp(member.joined_at.unwrap().timestamp(), 0);
+    let joined_discord = id.created_at();
+
+    msg.channel_id
+        .send_message(|m| {
+            m.embed(|e| {
+                e.title("User Information")
+                    .field("Username", username, true)
+                    .field("Nickname", nickname, true)
+                    .field("ID", id, true)
+                    .field("Discriminator", discriminator, true)
+                    .field("Joined The Server At", joined_at, true)
+                    .field("Joined Discord At", joined_discord, true)
+                    .field(
+                        "Roles",
+                        member
+                            .roles
+                            .iter()
+                            .map(|r| r.to_role_cached().unwrap().mention())
+                            .collect::<Vec<String>>()
+                            .join(" "),
+                        false,
+                    )
+                    .thumbnail(avatar_url)
+                    .color(0x000000)
+                    .footer(|f| f.text("Made by infobot"))
+            })
+        })
+        .unwrap();
 });
